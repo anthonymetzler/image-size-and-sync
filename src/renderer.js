3ce1965c2f0a0e8ef8dd73187ca26a0350b5e60b
Anthony Metzler <anthony.metzler@gmail.com>
2019-10-21 10:58:13 -0700

    Finalize implementation go Jimp and bug fixes

--- src/renderer.js
+++ src/renderer.js
@@ -26,7 +26,7 @@ ipc.on('select-output', (event, path) => {
   document.getElementById('selected-output').innerHTML = path;
 });
 
-const processImages = () => {
+const processImages = async () => {
   const fs = require('fs');
   const path = require('path');
   const os = require('os');
@@ -90,194 +90,192 @@ const processImages = () => {
     }
   }
 
-  fs.readdir(srcDir, (error, files) => {
-    if (error) {
-      ipc.send('show-error-dialog', ['Error: Source directory is not accessible.', `Error: ${error}`]);
-      process.exit(1);
-    }
+  const files = fs.readdirSync(srcDir);
 
-    const maxValue = files.length;
-    ipc.send('show-progressbar', [maxValue]);
+  if (!files) {
+    ipc.send('show-error-dialog', ['Error: Source directory is not accessible.', `Error: ${error}`]);
+    process.exit(1);
+  }
 
-    const syncTime = moment().format('YYYYMMDDHHmmss');
-    let filesProcessed = 0;
-    const errors = [];
+  const maxValue = files.length;
+  ipc.send('show-progressbar', [maxValue]);
 
-    files.forEach((file) => {
-    // for (let i = 0; i < files.length; i += 1) {
-      // const file = files[i];
-      const acceptedImageFormats = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tif'];
-      const fileExt = path.extname(file);
+  const syncTime = moment().format('YYYYMMDDHHmmss');
+  let filesProcessed = 0;
+  const errors = [];
 
-      if (!(acceptedImageFormats.includes(fileExt))) {
-        filesProcessed += 1;
-        ipc.send('process-progressbar');
-        // errors.push(`Skipped: File ${file}.`);
-        return;
-      }
+  // files.forEach((file) => {
+  for (let j = 0; j < files.length; j++) {
+    const acceptedImageFormats = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tif'];
+    const fileExt = path.extname(files[j]);
+    const renameExistingFileErrors = 0;
 
-      const fileNameExt = path.basename(file);
-      const fileName = path.parse(file).name;
-
-      let fileDir = null;
-      if (systemOS === 'win32') {
-        fileDir = `${srcDir}\\${file}`;
-      } else {
-        fileDir = `${srcDir}/${file}`;
-      }
-
-      let largeImageOutput = null;
-      let thumbImageOutput = null;
-      let smallThumbOutput = null;
-
-      if (systemOS === 'win32') {
-        largeImageOutput = `${largeImageDir}\\${fileName}-800px.${fileFormat}`;
-        thumbImageOutput = `${thumbImageDir}\\${fileName}-400px.${fileFormat}`;
-        smallThumbOutput = `${smallThumbDir}\\${fileName}-100px.${fileFormat}`;
-      } else {
-        largeImageOutput = `${largeImageDir}/${fileName}-800px.${fileFormat}`;
-        thumbImageOutput = `${thumbImageDir}/${fileName}-400px.${fileFormat}`;
-        smallThumbOutput = `${smallThumbDir}/${fileName}-100px.${fileFormat}`;
-      }
-
-      const existingLargeImage = fs.existsSync(largeImageOutput);
-      if (existingLargeImage) {
-        let renamedLargeImageOutput = null;
-        if (systemOS === 'win32') {
-          renamedLargeImageOutput = `${largeImageDir}\\${fileName}-800px-${syncTime}.${fileFormat}`;
-        } else {
-          renamedLargeImageOutput = `${largeImageDir}/${fileName}-800px-${syncTime}.${fileFormat}`;
-        }
-        try {
-          fs.renameSync(largeImageOutput, renamedLargeImageOutput);
-        } catch (err) { return errors.push(`Error: Failed renaming previous largeImage of ${fileNameExt}. ${err}`); }
-      }
-
-      const existingThumbImage = fs.existsSync(thumbImageOutput);
-      if (existingThumbImage) {
-        let renamedThumbImageOutput = null;
-        if (systemOS === 'win32') {
-          renamedThumbImageOutput = `${thumbImageDir}\\${fileName}-400px-${syncTime}.${fileFormat}`;
-        } else {
-          renamedThumbImageOutput = `${thumbImageDir}/${fileName}-400px-${syncTime}.${fileFormat}`;
-        }
-        try {
-          fs.renameSync(thumbImageOutput, renamedThumbImageOutput);
-        } catch (err) { return errors.push(`Error: Failed renaming previous thumbImage of ${fileNameExt}. ${err}`); }
-      }
-
-      const existingSmallThumbImage = fs.existsSync(smallThumbOutput);
-      if (existingSmallThumbImage) {
-        let renamedSmallThumbImageOutput = null;
-        if (systemOS === 'win32') {
-          renamedSmallThumbImageOutput = `${smallThumbDir}\\${fileName}-100px-${syncTime}.${fileFormat}`;
-        } else {
-          renamedSmallThumbImageOutput = `${smallThumbDir}/${fileName}-100px-${syncTime}.${fileFormat}`;
-        }
-        try {
-          fs.renameSync(smallThumbOutput, renamedSmallThumbImageOutput);
-        } catch (err) { return errors.push(`Error: Failed renaming previous smallThumbImage of ${fileNameExt}. ${err}`); }
-      }
-
-      // if (fileFormat === 'jpg') {
-      //   Jimp.read(fileDir, (err, img) => {
-      //     if (err) errors.push(`Error: Failed saving largeImage of ${fileNameExt}. ${err}`);
-      //     img
-      //       .background(0xFFFFFFFF)
-      //       .resize(Jimp.AUTO, 800) // resize
-      //       .quality(100) // set JPEG quality
-      //       .write(largeImageOutput); // save
-      //   });
-      //   Jimp.read(fileDir, (err, img) => {
-      //     if (err) errors.push(`Error: Failed saving thumbImage of ${fileNameExt}. ${err}`);
-      //     img
-      //       .background(0xFFFFFFFF)
-      //       .resize(Jimp.AUTO, 400) // resize
-      //       .quality(100) // set JPEG quality
-      //       .write(thumbImageOutput); // save
-      //   });
-      //   Jimp.read(fileDir, (err, img) => {
-      //     if (err) errors.push(`Error: Failed saving smallThumbImage of ${fileNameExt}. ${err}`);
-      //     img
-      //       .background(0xFFFFFFFF)
-      //       .resize(Jimp.AUTO, 100) // resize
-      //       .quality(100) // set JPEG quality
-      //       .write(smallThumbOutput); // save
-      //   });
-      // } else {
-      //   Jimp.read(fileDir, (err, img) => {
-      //     if (err) errors.push(`Error: Failed saving largeImage of ${fileNameExt}. ${err}`);
-      //     img
-      //       .resize(Jimp.AUTO, 800) // resize
-      //       .writeAsync(largeImageOutput); // save
-      //   });
-      //   Jimp.read(fileDir, (err, img) => {
-      //     if (err) errors.push(`Error: Failed saving thumbImage of ${fileNameExt}. ${err}`);
-      //     img
-      //       .resize(Jimp.AUTO, 400) // resize
-      //       .writeAsync(thumbImageOutput); // save
-      //   });
-      //   Jimp.read(fileDir, (err, img) => {
-      //     if (err) errors.push(`Error: Failed saving smallThumbImage of ${fileNameExt}. ${err}`);
-      //     img
-      //       .resize(Jimp.AUTO, 100) // resize
-      //       .writeAsync(smallThumbOutput); // save
-      //   });
-      // }
-
-
-      const image = nativeImage.createFromPath(fileDir);
-
-      if (image.isEmpty()) {
-        errors.push(`Error: Source image ${fileNameExt} is empty.`);
-      } else {
-        const resizedLargeImage = image.resize({ height: 800, quality: 'best' });
-        const resizedThumbImage = image.resize({ height: 400, quality: 'best' });
-        const resizedSmallThumbImage = image.resize({ height: 100, quality: 'best' });
-
-        let largeImage = null;
-        let thumbImage = null;
-        let smallThumbImage = null;
-
-        if (fileFormat === 'jpg') {
-          largeImage = resizedLargeImage.toJPEG(100);
-          thumbImage = resizedThumbImage.toJPEG(100);
-          smallThumbImage = resizedSmallThumbImage.toJPEG(100);
-        } else {
-          largeImage = resizedLargeImage.toPNG(1.0);
-          thumbImage = resizedThumbImage.toPNG(1.0);
-          smallThumbImage = resizedSmallThumbImage.toPNG(1.0);
-        }
-
-        fs.writeFile(largeImageOutput, largeImage, (err) => { if (err) errors.push(`Error: Failed saving largeImage of ${fileNameExt}. ${err}`); });
-        fs.writeFile(thumbImageOutput, thumbImage, (err) => { if (err) errors.push(`Error: Failed saving thumbImage of ${fileNameExt}. ${err}`); });
-        fs.writeFile(smallThumbOutput, smallThumbImage, (err) => { if (err) errors.push(`Error: Failed saving smallThumbImage of ${fileNameExt}. ${err}`); });
-      }
-
+    if (!(acceptedImageFormats.includes(fileExt))) {
       filesProcessed += 1;
       ipc.send('process-progressbar');
-    });
-    // }
+      continue;
+    }
 
-    if (errors.length > 0) {
-      let logDir = null;
+    const fileNameExt = path.basename(files[j]);
+    const fileName = path.parse(files[j]).name;
+
+    let fileDir = null;
+    if (systemOS === 'win32') {
+      fileDir = `${srcDir}\\${files[j]}`;
+    } else {
+      fileDir = `${srcDir}/${files[j]}`;
+    }
+
+    let largeImageOutput = null;
+    let thumbImageOutput = null;
+    let smallThumbOutput = null;
+
+    if (systemOS === 'win32') {
+      largeImageOutput = `${largeImageDir}\\${fileName}-800px.${fileFormat}`;
+      thumbImageOutput = `${thumbImageDir}\\${fileName}-400px.${fileFormat}`;
+      smallThumbOutput = `${smallThumbDir}\\${fileName}-100px.${fileFormat}`;
+    } else {
+      largeImageOutput = `${largeImageDir}/${fileName}-800px.${fileFormat}`;
+      thumbImageOutput = `${thumbImageDir}/${fileName}-400px.${fileFormat}`;
+      smallThumbOutput = `${smallThumbDir}/${fileName}-100px.${fileFormat}`;
+    }
+
+    const existingLargeImage = fs.existsSync(largeImageOutput);
+    if (existingLargeImage) {
+      let renamedLargeImageOutput = null;
       if (systemOS === 'win32') {
-        logDir = `${srcDir}\\errorLog-${syncTime}.json`;
+        renamedLargeImageOutput = `${largeImageDir}\\${fileName}-800px-${syncTime}.${fileFormat}`;
       } else {
-        logDir = `${srcDir}/errorLog-${syncTime}.json`;
+        renamedLargeImageOutput = `${largeImageDir}/${fileName}-800px-${syncTime}.${fileFormat}`;
+      }
+      try {
+        fs.renameSync(largeImageOutput, renamedLargeImageOutput);
+      } catch (err) { renameExistingFileErrors.push(`Error: Failed renaming previous largeImage of ${fileNameExt} so the file was skipped entirely from the size/sync process. ${err}`); }
+    }
+
+    const existingThumbImage = fs.existsSync(thumbImageOutput);
+    if (existingThumbImage) {
+      let renamedThumbImageOutput = null;
+      if (systemOS === 'win32') {
+        renamedThumbImageOutput = `${thumbImageDir}\\${fileName}-400px-${syncTime}.${fileFormat}`;
+      } else {
+        renamedThumbImageOutput = `${thumbImageDir}/${fileName}-400px-${syncTime}.${fileFormat}`;
+      }
+      try {
+        fs.renameSync(thumbImageOutput, renamedThumbImageOutput);
+      } catch (err) { renameExistingFileErrors.push(`Error: Failed renaming previous thumbImage of ${fileNameExt} so the file was skipped entirely from the size/sync process. ${err}`); }
+    }
+
+    const existingSmallThumbImage = fs.existsSync(smallThumbOutput);
+    if (existingSmallThumbImage) {
+      let renamedSmallThumbImageOutput = null;
+      if (systemOS === 'win32') {
+        renamedSmallThumbImageOutput = `${smallThumbDir}\\${fileName}-100px-${syncTime}.${fileFormat}`;
+      } else {
+        renamedSmallThumbImageOutput = `${smallThumbDir}/${fileName}-100px-${syncTime}.${fileFormat}`;
+      }
+      try {
+        fs.renameSync(smallThumbOutput, renamedSmallThumbImageOutput);
+      } catch (err) { renameExistingFileErrors.push(`Error: Failed renaming previous smallThumbImage of ${fileNameExt} so the file was skipped entirely from the size/sync process. ${err}`); }
+    }
+
+    if (renameExistingFileErrors.length > 0) {
+      errors.concat(renameExistingFileErrors);
+      filesProcessed += 1;
+      ipc.send('process-progressbar');
+      continue;
+    }
+
+    const srcImg = await Jimp.read(fileDir);
+
+    if (fileFormat === 'jpg') {
+      try {
+        await srcImg.background(0xFFFFFFFF).resize(Jimp.AUTO, 800).quality(100).writeAsync(largeImageOutput);
+      } catch (err) {
+        errors.push(`Error: Failed saving largeImage of ${fileNameExt}. ${err}`);
       }
 
-      const errorJSON = JSON.stringify(errors);
-      fs.writeFile(logDir, errorJSON, (err) => { if (err) ipc.send('show-error-dialog', ['Writing Error Log Failed!', `Tried writing the Error Log here: ${logDir}`]); });
-      ipc.send('show-error-dialog', ['Process completed with errors!', `Check Error Log here: ${logDir}`]);
+      try {
+        await srcImg.background(0xFFFFFFFF).resize(Jimp.AUTO, 400).quality(100).writeAsync(thumbImageOutput);
+      } catch (err) {
+        errors.push(`Error: Failed saving thumbImage of ${fileNameExt}. ${err}`);
+      }
+
+      try {
+        await srcImg.background(0xFFFFFFFF).resize(Jimp.AUTO, 100).quality(100).writeAsync(smallThumbOutput);
+      } catch (err) {
+        errors.push(`Error: Failed saving smallThumbImage of ${fileNameExt}. ${err}`);
+      }
+    } else {
+      try {
+        await srcImg.resize(Jimp.AUTO, 800).writeAsync(largeImageOutput);
+      } catch (err) {
+        errors.push(`Error: Failed saving largeImage of ${fileNameExt}. ${err}`);
+      }
+
+      try {
+        await srcImg.resize(Jimp.AUTO, 400).writeAsync(thumbImageOutput);
+      } catch (err) {
+        errors.push(`Error: Failed saving thumbImage of ${fileNameExt}. ${err}`);
+      }
+
+      try {
+        await srcImg.resize(Jimp.AUTO, 100).writeAsync(smallThumbOutput);
+      } catch (err) {
+        errors.push(`Error: Failed saving smallThumbImage of ${fileNameExt}. ${err}`);
+      }
     }
-  });
+
+    // const image = nativeImage.createFromPath(fileDir);
+
+    // if (image.isEmpty()) {
+    //   errors.push(`Error: Source image ${fileNameExt} is empty.`);
+    // } else {
+    //   const resizedLargeImage = image.resize({ height: 800, quality: 'best' });
+    //   const resizedThumbImage = image.resize({ height: 400, quality: 'best' });
+    //   const resizedSmallThumbImage = image.resize({ height: 100, quality: 'best' });
+
+    //   let largeImage = null;
+    //   let thumbImage = null;
+    //   let smallThumbImage = null;
+
+    //   if (fileFormat === 'jpg') {
+    //     largeImage = resizedLargeImage.toJPEG(100);
+    //     thumbImage = resizedThumbImage.toJPEG(100);
+    //     smallThumbImage = resizedSmallThumbImage.toJPEG(100);
+    //   } else {
+    //     largeImage = resizedLargeImage.toPNG(1.0);
+    //     thumbImage = resizedThumbImage.toPNG(1.0);
+    //     smallThumbImage = resizedSmallThumbImage.toPNG(1.0);
+    //   }
+
+    //   fs.writeFile(largeImageOutput, largeImage, (err) => { if (err) errors.push(`Error: Failed saving largeImage of ${fileNameExt}. ${err}`); });
+    //   fs.writeFile(thumbImageOutput, thumbImage, (err) => { if (err) errors.push(`Error: Failed saving thumbImage of ${fileNameExt}. ${err}`); });
+    //   fs.writeFile(smallThumbOutput, smallThumbImage, (err) => { if (err) errors.push(`Error: Failed saving smallThumbImage of ${fileNameExt}. ${err}`); });
+    // }
+
+    filesProcessed += 1;
+    ipc.send('process-progressbar');
+  }
+
+  if (errors.length > 0) {
+    let logDir = null;
+    if (systemOS === 'win32') {
+      logDir = `${srcDir}\\errorLog-${syncTime}.json`;
+    } else {
+      logDir = `${srcDir}/errorLog-${syncTime}.json`;
+    }
+
+    const errorJSON = JSON.stringify(errors);
+    fs.writeFile(logDir, errorJSON, (err) => { if (err) ipc.send('show-error-dialog', ['Writing Error Log Failed!', `Tried writing the Error Log here: ${logDir}`]); });
+    ipc.send('show-error-dialog', ['Process completed with errors!', `Check Error Log here: ${logDir}`]);
+  }
 
   setTimeout(() => {
     ipc.send('set-progressbar-completed');
   }, 5000);
 };
 
-document.querySelector('#start-process').addEventListener('click', () => {
-  processImages();
+document.querySelector('#start-process').addEventListener('click', async () => {
+  await processImages();
 });
